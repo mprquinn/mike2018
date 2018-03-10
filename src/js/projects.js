2ff5560d71cb6f483ff4e5d506b90292ac572f9d
Mike Quinn <Ask>
2018-03-09 20:21:08 -0700

    new projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -15,7 +15,7 @@ class Project extends React.Component {
           description:
             "Front-End Development for Moxie's as part of the Simple Simple Ads development team. A smidgen of back-end work as well. 2015.",
           technologies: ["Drupal", "SASS", "JS", "PHP"],
-          image: "dist/images/project-moxies-duotone.png",
+          image: "dist/images/project-moxies.jpg",
           url: "https://www.moxies.com"
         },
         {
@@ -23,22 +23,15 @@ class Project extends React.Component {
           description:
             "Full site build for Local 510. Design by Concorde Group as part of the Simple Simple Ads development team. 2017.",
           technologies: ["Drupal", "SASS", "JS", "Babel", "Gulp", "PHP"],
-          image: "dist/images/project-local510-duotone.png",
+          image: "dist/images/project-local510.jpg",
           url: "https://local510.ca"
         },
         {
           title: "Spredfast",
           description:
             "Miscellaneous components and pages for the Spredfast website as part of the Simple Simple Ads development team. Notably landing page banners. 2017 - 2018.",
-          technologies: [
-            "Drupal",
-            "SASS",
-            "JS",
-            "Babel",
-            "Grunt",
-            "Figma/Sketch"
-          ],
-          image: "dist/images/project-spredfast-duotone.jpg",
+          technologies: ["Drupal", "SASS", "JS", "Grunt", "Figma/Sketch"],
+          image: "dist/images/project-spredfast.jpg",
           url: "https://spredfast.com"
         },
         {
@@ -46,8 +39,32 @@ class Project extends React.Component {
           description:
             "Full theme build and front-end interactive elements developed for Chop Steakhouse's website as part of the Simple Simple Ads development team. 2016.",
           technologies: ["Drupal", "SASS", "JS", "Babel", "Gulp"],
-          image: "dist/images/project-chop-duotone.jpg",
+          image: "dist/images/project-chop.jpg",
           url: "https://chop.ca"
+        },
+        {
+          title: "Boulevard",
+          description:
+            "Full theme build for Boulevard Vancouver's website as part of the Simple Simple Ads development team. 2016.",
+          technologies: ["Drupal", "SASS", "JS", "Gulp"],
+          image: "dist/images/project-boulevard.jpg",
+          url: "https://boulevardvancouver.ca"
+        },
+        {
+          title: "Save Playlists",
+          description:
+            "Personal learning project. React app to save auto-generated Spotify playlists.",
+          technologies: ["React", "SASS", "Babel", "Spotify API"],
+          image: "dist/images/project-save-playlists.jpg",
+          url: "https://www.save-playlists.com"
+        },
+        {
+          title: "PWLS",
+          description:
+            "Personal learning project. React app for our bookclub. For scheduling meetups and suggesting books.",
+          technologies: ["React", "SASS", "Babel", "Google Books API", "Firebase"],
+          image: "dist/images/project-pwls.jpg",
+          url: "https://prestigeworldwideliterary.herokuapp.com/"
         }
       ]
     };
@@ -175,21 +192,19 @@ class Project extends React.Component {
     img.src = image;
   }
   componentWillMount() {
-    
     const _this = this;
     const length = this.state.projects.length;
     let loaded = 0;
-    this.state.projects.forEach(
-      project => {
-        const image = project.image;
-        _this.loadImages(image);
-        loaded++;
-        if (loaded === length) {
-          this.setState({
-            loaded: true
-          });
-        }
-      });
+    this.state.projects.forEach(project => {
+      const image = project.image;
+      _this.loadImages(image);
+      loaded++;
+      if (loaded === length) {
+        this.setState({
+          loaded: true
+        });
+      }
+    });
   }
   componentDidMount() {
     const landing = document.querySelector(".landing");
